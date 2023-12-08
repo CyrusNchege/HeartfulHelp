83855215c4ceeaaa7d9a8411aadeb17bee27255c
CyrusNchege <cyruschegecc@gmail.com>
2023-12-08 09:18:57 +0300

    Add django project setup and handle Secret key

---
+++ heartfulHelp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for heartfulHelp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'heartfulHelp.settings')
+
+application = get_asgi_application()
