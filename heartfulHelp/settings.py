1bed43aa629b9d4700e0fbd67cbaafd8f227fb88
CyrusNchege <cyruschegecc@gmail.com>
2023-12-08 11:23:16 +0300

    Add donations module and setup templates folder

--- heartfulHelp/settings.py
+++ heartfulHelp/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'donations',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -53,7 +56,7 @@ ROOT_URLCONF = 'heartfulHelp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
