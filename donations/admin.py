3b1292a0b259d7e21041ffcf80b299a1fdebe4dd
CyrusNchege <cyruschegecc@gmail.com>
2023-12-10 15:37:38 +0300

    Add confcauses to the templates

--- donations/admin.py
+++ donations/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import FundraiseCause, Donation
+
+admin.site.register(FundraiseCause)
+admin.site.register(Donation)
--- donations/views.py
+++ donations/views.py
@@ -11,3 +11,50 @@ def home(request):
 def about(request):
     return render(request, 'donations/about.html')
 
+def causes_list(request):
+    causes = FundraiseCause.objects.all()
+    return render(request, 'donations/causes_list.html', {'causes': causes})
+
+def cause_detail(request, pk):
+    cause = FundraiseCause.objects.get(id=pk)
+    return render(request, 'donations/cause_detail.html', {'cause': cause})
+
+def cause_create(request):
+    if request.method == 'POST':
+        form = FundraiseCauseForm(request.POST, request.FILES)
+        if form.is_valid():
+            cause = form.save()
+            return redirect('cause_detail', pk=cause.pk)
+    else:
+        form = FundraiseCauseForm()
+    return render(request, 'donations/cause_form.html', {'form': form})
+
+def cause_edit(request, pk):
+    cause = FundraiseCause.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = FundraiseCauseForm(request.POST, request.FILES, instance=cause)
+        if form.is_valid():
+            cause = form.save()
+            return redirect('cause_detail', pk=cause.pk)
+    else:
+        form = FundraiseCauseForm(instance=cause)
+    return render(request, 'donations/cause_form.html', {'form': form})
+
+def cause_delete(request, pk):
+    FundraiseCause.objects.get(id=pk).delete()
+    return redirect('causes_list')
+
+def donation_create(request, pk):
+    cause = FundraiseCause.objects.get(id=pk)
+    if request.method == 'POST':
+        form = DonationForm(request.POST)
+        if form.is_valid():
+            donation = form.save(commit=False)
+            donation.cause = cause
+            donation.save()
+            cause.current_amount += donation.amount
+            cause.save()
+            return redirect('cause_detail', pk=cause.pk)
+    else:
+        form = DonationForm()
+    return render(request, 'donations/donation_form.html', {'form': form})
