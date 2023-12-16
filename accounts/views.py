a800d12371330141ff3698ec6782105229246c78
CyrusNchege <cyruschegecc@gmail.com>
2023-12-16 13:34:57 +0300

    Add messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import CustomUserCreationForm, CustomUserAuthenticationForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
@@ -9,7 +10,7 @@ def register (request):
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            return redirect('home')
+            return redirect('login')
     else:
         form = CustomUserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
@@ -23,7 +24,10 @@ def loginpage (request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('home')
+
+                messages.success(request, f'Successfully logged in as {username}')
+                return redirect('dashboard')
+                
             else:
                 form.add_error(None, 'Invalid username or password')
     else:
@@ -32,7 +36,8 @@ def loginpage (request):
 
 def logout_view(request):
     logout(request)
-    return redirect('home')
+    messages.success(request, 'Successfully logged out')
+    return redirect('login')
 
 def dashboard(request):
 
