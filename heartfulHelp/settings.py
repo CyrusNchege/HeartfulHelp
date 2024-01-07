43aeea4029e28f13c53fefc9f5ca27d8188d34fc
CyrusNchege <cyruschegecc@gmail.com>
2024-01-07 22:13:09 +0300

    Update SECRET_KEY and DEBUG settings

--- heartfulHelp/settings.py
+++ heartfulHelp/settings.py
@@ -20,14 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-try:
-    SECRET_KEY=os.getenv("SECRET_KEY")
-except KeyError:
-    with open("/home/ubuntu/HeartfulHelp/secrets.txt") as f:
-        SECRET_KEY = f.read().strip()
+SECRET_KEY=os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG=os.getenv("DEBUG")
+DEBUG=os.environ.get("DEBUG")
 ALLOWED_HOSTS=['localhost', '127.0.0.1', '3.91.243.40']
 
 
