cd9ac398e8c3f5694b2bbaa2c5f52b8ea5b8dd07
CyrusNchege <cyruschegecc@gmail.com>
2024-01-08 18:07:21 +0300

    Add staticfiles_urlpatterns and serve view for media files

--- heartfulHelp/urls.py
+++ heartfulHelp/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,6 +27,8 @@ urlpatterns = [
     path("", include("accounts.urls")),
     path('accounts/', include('allauth.urls')),
     path('accounts/', include('allauth.socialaccount.urls')),
+
 ]  
-if settings.DEBUG :
-        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
