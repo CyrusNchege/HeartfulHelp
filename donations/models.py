fb82f0d8db4e726f4af0fd4db9067e1ea74302db
CyrusNchege <cyruschegecc@gmail.com>
2024-01-15 12:06:24 +0300

    Fix overiding default image

--- donations/models.py
+++ donations/models.py
@@ -4,19 +4,17 @@ from accounts.models import CustomUser
 
 # Create your models here.
 
+def default_image():
+    return 'cause_images/default.webp'
 class FundraiseCause(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     goal = models.IntegerField()
     current_amount = models.IntegerField(default=0, validators=[MinValueValidator(0)])
     account_number = models.IntegerField(null=False, blank=False)
-    image = models.ImageField( upload_to='cause_images', null=True, blank=True)
+    image = models.ImageField( upload_to='cause_images', null=True, blank=True, default=default_image)
     creator = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name  = 'causes')
-                              
-    def save (self, *args, **kwargs):
-        if self.image is not None:
-            self.image.name = 'cause_images/default.webp'
-        super().save(*args, **kwargs)
+
 
     def __str__(self):
         return self.title
