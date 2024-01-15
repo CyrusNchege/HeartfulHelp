d55dc4cec4649a188400c6431b2daca065cb55c5
CyrusNchege <cyruschegecc@gmail.com>
2024-01-15 11:32:26 +0300

    Add diffrent views for logged in users and those unauthenticated

--- donations/mpesa.py
+++ donations/mpesa.py
@@ -6,21 +6,23 @@ from requests.auth import HTTPBasicAuth
 import os
 
 def get_mpesa_access_token():
-    consumer_key = os.environ.get('MPESA_CONSUMER_KEY')
-    consumer_secret = os.environ.get('MPESA_CONSUMER_SECRET')
+    consumer_key = os.getenv('MPESA_CONSUMER_KEY')
+    consumer_secret = os.getenv('MPESA_CONSUMER_SECRET')
     api_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
 
     r = requests.get(api_url, auth=HTTPBasicAuth(consumer_key, consumer_secret))
     mpesa_access_token = json.loads(r.text)
-    return mpesa_access_token['access_token']
+    validated_mpesa_access_token = mpesa_access_token['access_token']
+    return validated_mpesa_access_token
 
 def generate_mpesa_password():
     lipa_time = datetime.now().strftime('%Y%m%d%H%M%S')
-    business_short_code = os.environ.get('MPESA_SHORTCODE')
-    passkey = os.environ.get('MPESA_PASSKEY')
+    business_short_code = os.getenv('MPESA_SHORTCODE')
+    passkey = os.getenv('MPESA_PASSKEY')
     data_to_encode = business_short_code + passkey + lipa_time
     online_password = base64.b64encode(data_to_encode.encode())
-    return online_password.decode('utf-8')
+    decoded_password = online_password.decode('utf-8')
+    return decoded_password
 
 def initiate_stk_push(phone_number, amount):
     access_token = get_mpesa_access_token()
@@ -32,12 +34,12 @@ def initiate_stk_push(phone_number, amount):
     }
 
     request_payload = {
-        "BusinessShortCode": os.environ.get('MPESA_SHORTCODE'),
+        "BusinessShortCode": os.getenv('MPESA_SHORTCODE'),
         "Password": generate_mpesa_password(),
         "Timestamp": datetime.now().strftime('%Y%m%d%H%M%S'),
         "TransactionType": "CustomerPayBillOnline",
         "Amount": amount,
-        "PartyA": os.environ.get('MPESA_PARTY_A'),
+        "PartyA": os.getenv('MPESA_PARTY_A'),
         "PartyB": 174379,
         "PhoneNumber": phone_number,
         "CallBackURL": "https://sandbox.safaricom.co.ke/mpesa/",
